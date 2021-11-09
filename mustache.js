cbe5b5b8447ec91375cdb0ea0f085df800632dc1
Virsandhu <80320001+Virsandhu@users.noreply.github.com>
2021-11-09 14:39:55 +0530

    Add files via upload

---
+++ mustache.js
@@ -0,0 +1,13 @@
+function preload(){
+}
+
+function setup(){
+    canvas_frame = createCanvas(300,300);
+    canvas_frame.center();
+}
+function draw(){
+}
+
+function take_snapshot(){
+    save('my-mustache-filter')
+}
